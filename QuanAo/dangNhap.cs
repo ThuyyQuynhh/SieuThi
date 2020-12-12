e91518ea283c2c392e986844c71368406d1f2929
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-12-12 09:28:14 +0700

    TQ_DN_Tao giao dien trang dang nhap

---
+++ QuanAo/dangNhap.cs
@@ -0,0 +1,22 @@
+﻿using QuanAo.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanAo
+{
+    public partial class login : DevExpress.XtraEditors.XtraForm
+    {
+        public login()
+        {
+            InitializeComponent();
+           
+        }
+        
+    }
+}
