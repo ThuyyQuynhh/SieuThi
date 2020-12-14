2ea3c13897032f82bbdd1db9d5f710a0251100e1
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-14 14:52:29 +0700

    BT_Tao form thong ke doanh thu

---
+++ QuanAo/ThongkeDoanhthu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanAo.Data;
+
+namespace QuanAo
+{
+    public partial class ThongkeDoanhthu : DevExpress.XtraEditors.XtraUserControl
+    {
+        public ThongkeDoanhthu()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
