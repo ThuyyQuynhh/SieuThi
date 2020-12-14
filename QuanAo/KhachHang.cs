618f396d57f95993def08503f3a719d90ebd2672
ThuyyQuynhh <nguyenthuyquynhcvp@gmail.com>
2020-12-14 10:32:07 +0700

    TQ_KH_Tao giao dien trang quan ly khach hang

---
+++ QuanAo/KhachHang.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanAo.Data;
+using DevExpress.XtraReports.UI;
+
+namespace QuanAo
+{
+    public partial class KhachHang : DevExpress.XtraEditors.XtraUserControl
+    {
+       dataProvider dataProvider = new dataProvider();
+        public KhachHang()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        
+        
+    }
+}
