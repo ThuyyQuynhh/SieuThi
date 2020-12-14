c598193ac2db1dea6295800b531e39d17a389d50
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-12-14 21:16:11 +0700

    TL_ Tao report hoa don

---
+++ QuanAo/ReportHD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace QuanAo
+{
+    public partial class ReportHD : DevExpress.XtraReports.UI.XtraReport
+    {
+        public ReportHD()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
