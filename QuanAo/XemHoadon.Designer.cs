ee91d4b4ce94c94e3a2cbbbda213fa5a19f360c5
tronglinhmta0611@gmail.com <tronglinhmta0611@gmail.com>
2020-12-15 11:17:02 +0700

    TL_ Tao report xem hoa don

---
+++ QuanAo/XemHoadon.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace QuanAo
+{
+    partial class XemHoadon
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.datahoadon = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.denngay = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tungay = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datahoadon)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.datahoadon);
+            this.groupControl1.Controls.Add(this.panel2);
+            this.groupControl1.Controls.Add(this.panel1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(1282, 607);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "Xem hóa đơn";
+            // 
+            // datahoadon
+            // 
+            this.datahoadon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.datahoadon.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.datahoadon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datahoadon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.datahoadon.Location = new System.Drawing.Point(2, 92);
+            this.datahoadon.Name = "datahoadon";
+            this.datahoadon.RowHeadersWidth = 51;
+            this.datahoadon.RowTemplate.Height = 24;
+            this.datahoadon.Size = new System.Drawing.Size(1278, 425);
+            this.datahoadon.TabIndex = 6;
+            this.datahoadon.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datahoadon_CellClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.simpleButton1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(2, 517);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1278, 88);
+            this.panel2.TabIndex = 5;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(17, 13);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(177, 64);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "In hóa đơn";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.simpleButton2);
+            this.panel1.Controls.Add(this.denngay);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tungay);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(2, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1278, 67);
+            this.panel1.TabIndex = 4;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(731, 22);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(148, 31);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Tìm kiếm";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // denngay
+            // 
+            this.denngay.Location = new System.Drawing.Point(478, 28);
+            this.denngay.Name = "denngay";
+            this.denngay.Size = new System.Drawing.Size(200, 23);
+            this.denngay.TabIndex = 3;
+            this.denngay.Value = new System.DateTime(2020, 4, 12, 8, 26, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(367, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Đến ngày:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Từ ngày:";
+            // 
+            // tungay
+            // 
+            this.tungay.Location = new System.Drawing.Point(129, 31);
+            this.tungay.Name = "tungay";
+            this.tungay.Size = new System.Drawing.Size(200, 23);
+            this.tungay.TabIndex = 2;
+            this.tungay.Value = new System.DateTime(2020, 4, 12, 8, 26, 0, 0);
+            // 
+            // XemHoadon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupControl1);
+            this.Name = "XemHoadon";
+            this.Size = new System.Drawing.Size(1282, 607);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.datahoadon)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private System.Windows.Forms.DateTimePicker denngay;
+        private System.Windows.Forms.DateTimePicker tungay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.Panel panel1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private System.Windows.Forms.DataGridView datahoadon;
+    }
+}
---
+++ QuanAo/XemHoadon.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QuanAo.Data;
+using DevExpress.XtraReports.UI;
+
+namespace QuanAo
+{
+    public partial class XemHoadon : DevExpress.XtraEditors.XtraUserControl
+    {
+        public XemHoadon()
+        {
+            InitializeComponent();
+        }
+        dataProvider dataProvider = new dataProvider();
+
+
+
+        public int click = 0;
+       
+
+
+        // tìm kiếm hóa đơn
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            string query = string.Format("select *from HoaDon HD where HD.Ngaytao between '{0}' and '{1}'", tungay.Value, denngay.Value);
+
+            datahoadon.DataSource = dataProvider.GetDataTable(query);
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (click == 1)
+            {
+
+                //show ra hoá đơn tương ứng với hàng được chọn
+                int i = datahoadon.CurrentRow.Index;
+                ReportHD report = new ReportHD();
+
+
+                string query = "exec Report '" + datahoadon.Rows[i].Cells[0].Value.ToString()+ "'";
+                DataTable dataHD = dataProvider.GetDataTable(query);
+                report.DataSource = dataHD;
+                report.ShowPreviewDialog();
+            }
+        }
+
+        private void datahoadon_CellClick(object sender, DataGridViewCellEventArgs e) 
+        {
+            click = 1; //tức là đã chọn ra một hàng để xem hoá đơn
+        }
+    }
+}
