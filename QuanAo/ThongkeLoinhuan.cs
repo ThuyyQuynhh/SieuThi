b8a2dc00139e4350b0e12b971d72643f62dc479f
mrle0bda3t <thaibatuyen2081999@gmail.com>
2020-12-17 14:03:40 +0700

    BT_Bat su kien button thong ke

--- QuanAo/ThongkeLoinhuan.cs
+++ QuanAo/ThongkeLoinhuan.cs
@@ -44,5 +44,45 @@ namespace QuanAo
             chon = 2;
         }
 
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            if(chon == 0)
+            {
+                
+                string query = string.Format("select HD.MaHD,CT.MaSP, SP.TenSP, CT.SLBan, SP.Gia ,SP.Loinhuan, (SP.Gia*SP.Loinhuan) as Thuve " +
+                    "from HoaDon HD, ChiTiet CT, SanPham SP where HD.MaHD = CT.MaHD and CT.MaSP = SP.MaSP and  HD.NgayTao = '{0}'", dtpChonngay.Value);
+
+                dtgvLoinhuan.DataSource = dataProvider.GetDataTable(query);
+                dtpChonngay.Enabled = false;
+            }
+            if(chon == 1)
+            {
+                
+                string query = string.Format("select HD.MaHD,CT.MaSP, SP.TenSP, CT.SLBan, SP.Gia ,SP.Loinhuan, (SP.Gia*SP.Loinhuan) as Thuve " +
+                    "from HoaDon HD, ChiTiet CT, SanPham SP where HD.MaHD = CT.MaHD and CT.MaSP = SP.MaSP and MONTH(HD.NgayTao) = '{0}' and YEAR(HD.NgayTao) = '{1}'", cmbChonthang.Text, cmbChonnam.Text);
+
+                dtgvLoinhuan.DataSource = dataProvider.GetDataTable(query);
+                
+                cmbChonthang.Enabled = false;
+                cmbChonnam.Enabled = false;
+            }
+            if(chon == 2)
+            {
+                
+                string query = string.Format("select HD.MaHD,CT.MaSP, SP.TenSP, CT.SLBan, SP.Gia ,SP.Loinhuan, (SP.Gia*SP.Loinhuan) as Thuve " +
+                    "from HoaDon HD, ChiTiet CT, SanPham SP where HD.MaHD = CT.MaHD and CT.MaSP = SP.MaSP and YEAR(HD.NgayTao) = '{0}'", cmbChonnam.Text);
+
+                dtgvLoinhuan.DataSource = dataProvider.GetDataTable(query);
+                
+                cmbChonnam.Enabled = false;
+            }
+            int loinhuan = 0;
+            for (int i = 0; i < dtgvLoinhuan.RowCount; i++)
+            {
+                loinhuan = loinhuan + Convert.ToInt32(dtgvLoinhuan.Rows[i].Cells[6].Value);
+            }
+            txbLoinhuan.Text = loinhuan.ToString();
+
+        }
     }
 }
